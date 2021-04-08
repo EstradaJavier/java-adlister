7a05ab2440a6ef5327280eebea21b4860aba1609
javier estrada <estrada.javier.2022@gmail.com>
2021-04-08 10:12:02 -0500

    saving HelloWorld servlet

--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -3,14 +3,36 @@ import javax.servlet.*;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.*;
 
-@WebServlet(name = "HelloWorldServlet", urlPatterns = "/hello-world")
+@WebServlet(name = "HelloWorldServlet", urlPatterns = "/Javier")
 public class HelloWorldServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        response.setContentType("text/html");
         PrintWriter out = response.getWriter();
-        out.println("<h1>Hello, World!</h1>");
+        response.setContentType("text/html");
+
+        String output = "";
+        output += "<h1>Login Page</h1>";
+        output += "<form action='/login' method='POST'>";
+        output += "<input name='username' placeholder='Enter UserName: ' />";
+        output += "<button>Submit</button>";
+        output += "</form>";
+        out.println(output);
+
+
+
+//        PrintWriter out = response.getWriter();
+//        out.println("<h1>Hello, Javier!</h1>");
+//        out.println("<h1>Hello World!</h1><a href='/about'>About Page</a>");
+
+
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)  throws ServletException, IOException {
+        System.out.println("LOGIN REQUEST SUBMITTED");
+        response.sendRedirect("/Hello");
     }
 
 }
