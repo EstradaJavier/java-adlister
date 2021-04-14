0ad14686f001c704e8b6e5258399404c1696231d
javier estrada <estrada.javier.2022@gmail.com>
2021-04-14 12:18:11 -0500

    first save of sessions-and-cookies exercise

--- src/main/java/LoginServlet.java
+++ src/main/java/LoginServlet.java
@@ -5,6 +5,15 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+
+//TODO: Lock down your profile page
+//
+//        Ensure that users can only visit the /profile page if they are logged in
+//        In your LoginServlet, when a user successfully logs in, set a key named "user" in the session
+//        Use this key to redirect unauthorized users that try to visit the /profile page
+//        If a user is already logged in and they visit /login, instead redirect them to their profile page
+
+
 @WebServlet(name = "LoginServlet", urlPatterns = "/login")
 public class LoginServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
